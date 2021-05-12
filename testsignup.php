5998605e87d6f30c0aae48a9838ac5a72030fb0d
moblog123 <83898035+moblog123@users.noreply.github.com>
2021-05-12 10:25:21 +0530

    Update testsignup.php

--- testsignup.php
+++ testsignup.php
@@ -128,7 +128,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-secondary ml-2" value="Reset">
             </div>
-            <p>Already have an account? <a href="testlogin.php">Login here</a>.</p>
+            <p>Already have an account? <a href="/cake/testlogin.php">Login here</a>.</p>
         </form>
     </div>    
 </body>
